0cff520a4d5620840d4aecd6de599ac40912cad7
Marc <marcbaque1311@gmail.com>
2021-06-26 00:22:56 +0200

    [ ADD ] basic header component

--- resources/src/App.vue
+++ resources/src/App.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
+TheHeader
 p This is App!
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
+import TheHeader from "./components/layout/TheHeader.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    TheHeader,
+  },
   setup: () => {
     async function loadSchema() {
       const res = await fetch("./schemas/cloud-property-schema.json");
---
+++ resources/src/components/layout/TheHeader.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+header.the-header
+  h1 Cloud Editor
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheHeader",
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/_variables' as *;
+
+.the-header {
+  padding: 0.5em 2em;
+  background-color: $color-element;
+}
+
+h1 {
+  font-size: 1.25em;
+}
+</style>
