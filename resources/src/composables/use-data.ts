0fc13ee394f33805d8abc69a52c9a40627fc3235
Marc <marcbaque1311@gmail.com>
2021-06-26 20:07:51 +0200

    [ REFACTOR ] file loading

--- resources/src/composables/use-data.ts
+++ resources/src/composables/use-data.ts
@@ -1,20 +1,37 @@
 import { computed, ref, Ref } from 'vue';
 import { TheInrealCloudPropertySchema } from '../types/property-schema';
 const currentFile: Ref<TheInrealCloudPropertySchema | null> = ref(null);
+const currentFileName = ref('');
 const fileLoaded = computed(() => currentFile.value != null);
 
 export const useData = () => {
-  async function openFile() {
+  async function openJsonFile() {
     // @ts-ignore uses API that only works in Google Chrome
-    const [fileHandle] = await window.showOpenFilePicker();
-    const file = await fileHandle.getFile();
-    const contents = await file.text();
-    currentFile.value = JSON.parse(contents);
+    try {
+      const [fileHandle] = await window.showOpenFilePicker({
+        types: [
+          {
+            description: 'JSON Files',
+            accept: {
+              'application/json': ['.json'],
+            },
+          },
+        ],
+        excludeAcceptAllOption: true,
+      });
+      const file = await fileHandle.getFile();
+      const contents = await file.text();
+      currentFileName.value = file.name;
+      currentFile.value = JSON.parse(contents);
+    } catch (e: unknown) {
+      console.log((e as Error).message);
+    }
   }
 
   return {
-    openFile,
+    openJsonFile,
     currentFile,
+    currentFileName,
     fileLoaded,
   };
 };
