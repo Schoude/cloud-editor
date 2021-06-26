50c120d8597887a9ed8b6b812ee78d2140e58134
Marc <marcbaque1311@gmail.com>
2021-06-27 01:05:58 +0200

    [ UPDATE ] extend the property schema interface

--- resources/src/types/property-schema.ts
+++ resources/src/types/property-schema.ts
@@ -2,8 +2,28 @@ export interface TheInrealCloudPropertySchema {
   definitions: {
     [key: string]: {
       description: string;
+      type: 'object';
+      required: string[];
+      properties: {
+        [key: string]: {
+          id: {
+            $ref: string;
+          };
+          guid: {
+            $ref: string;
+          };
+        };
+      };
     };
   };
-
-  properties: {};
+  properties: {
+    [key: string]: {
+      $id: string;
+      $ref?: string;
+      type?: 'array';
+      items?: {
+        $ref: string;
+      };
+    };
+  };
 }
