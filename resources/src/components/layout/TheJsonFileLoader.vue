f4409545cb29a0be031384c155bb76419ed6435d
Marc <marcbaque1311@gmail.com>
2021-06-26 20:11:39 +0200

    [ ADD ] TheJsonFileLoader component

---
+++ resources/src/components/layout/TheJsonFileLoader.vue
@@ -0,0 +1,37 @@
+
+<template lang="pug">
+.the-json-file-loader
+  BaseButton(@click='openJsonFile') JSON-Datei öffnen
+  span.loaded-file__name(v-if='fileLoaded') <b>{{ currentFileName }}</b> geladen <small>(Schema-Version: {{ currentFile?.version }})</small>.
+  span.loaded-file__name(v-else) Keine Datei geladen.
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseButton from '../buttons/BaseButton.vue';
+import { useData } from '../../composables/use-data';
+
+export default defineComponent({
+  name: 'TheJsonFileLoader',
+  components: {
+    BaseButton,
+  },
+  setup: () => {
+    const { openJsonFile, currentFile, currentFileName, fileLoaded } =
+      useData();
+
+    return {
+      openJsonFile,
+      currentFile,
+      currentFileName,
+      fileLoaded,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.loaded-file__name {
+  margin-left: 1em;
+}
+</style>
--- resources/src/pages/Home.vue
+++ resources/src/pages/Home.vue
@@ -1,25 +1,16 @@
 <template lang="pug">
 section.home
-  BaseButton(@click='openFile') JSON-Datei öffnen
+  TheJsonFileLoader
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import BaseButton from '../components/buttons/BaseButton.vue';
-import { useData } from '../composables/use-data';
+import TheJsonFileLoader from '../components/layout/TheJsonFileLoader.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    BaseButton,
-  },
-  setup: () => {
-    const { openFile, currentFile } = useData();
-
-    return {
-      openFile,
-      currentFile,
-    };
+    TheJsonFileLoader,
   },
 });
 </script>
