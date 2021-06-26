bc0a57067f763584f01a1d90d0d1fcdbb30f6fe9
Marc <marcbaque1311@gmail.com>
2021-06-27 00:57:42 +0200

    [ ADD ] list initial property keys with descrioption

--- resources/src/pages/Edit.vue
+++ resources/src/pages/Edit.vue
@@ -1,12 +1,35 @@
 <template lang="pug">
-section.edit Edit
+section.edit
+  p(v-for='property of properties') 
+    <span :title='getDescription(property)'>ℹ️</span> {{ property }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useValidation } from '../composables/use-validation';
 
 export default defineComponent({
   name: 'Edit',
+  setup: () => {
+    const { schema } = useValidation();
+    const properties = computed(() =>
+      Object.keys(schema.value?.properties as {})
+    );
+
+    function formatProperty(property: string): string {
+      return property.endsWith('s') ? property.replace('s', '') : property;
+    }
+
+    function getDescription(property: string) {
+      return schema.value?.definitions[formatProperty(property)]?.description;
+    }
+
+    return {
+      schema,
+      properties,
+      getDescription,
+    };
+  },
 });
 </script>
 
