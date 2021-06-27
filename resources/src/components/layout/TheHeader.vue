f440032ba549af86623f38a8bf7697971c209aaa
Marc <marcbaque1311@gmail.com>
2021-06-27 02:23:48 +0200

    [ ADD ] show the currentfile on pages that are not home in the header

--- resources/src/components/layout/TheHeader.vue
+++ resources/src/components/layout/TheHeader.vue
@@ -1,13 +1,31 @@
 <template lang="pug">
 header.the-header
   h1 Cloud Editor
+  span.current-file__name(v-if='showFileName') geladene Datei: <b>{{ currentFileName }}</b>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { useData } from '../../composables/use-data';
+import { ROUTE_NAMES } from '../../router/routing-info';
 
 export default defineComponent({
-  name: "TheHeader",
+  name: 'TheHeader',
+  setup: () => {
+    const router = useRouter();
+    const { currentFileName, fileLoaded } = useData();
+
+    const showFileName = computed(
+      () => fileLoaded && router.currentRoute.value.name !== ROUTE_NAMES.HOME
+    );
+
+    return {
+      currentFileName,
+      fileLoaded,
+      showFileName,
+    };
+  },
 });
 </script>
 
@@ -17,9 +35,17 @@ export default defineComponent({
 .the-header {
   padding: 0.5em 2em;
   background-color: $color-element;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  // column-gap: 3em;
 }
 
 h1 {
   font-size: 1.25em;
+}
+
+.current-file__name {
+  font-size: 14px;
 }
 </style>
