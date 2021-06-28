5537105d870ce0706f5602e8337bec9c6642df98
Marc <marcbaque1311@gmail.com>
2021-06-28 21:22:36 +0200

    [ ADD ] initial component strcuture for merge

--- resources/src/pages/Merge.vue
+++ resources/src/pages/Merge.vue
@@ -1,14 +1,41 @@
 <template lang="pug">
-section.merge Merge
+section.merge
+  h1.title CSV to JSON-Merger
+  .meta-info
+    .file-selection
+      BaseButton Wähle eine CSV-Datei zum Mergen aus
+    .loaded-json
+      h2.title-filename JSON-Datei, in die gemerged wird
+      .filename {{ currentFileName }}
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BaseButton from '../components/buttons/BaseButton.vue';
+import { useData } from '../composables/use-data';
 
 export default defineComponent({
   name: 'Merge',
+  components: {
+    BaseButton,
+  },
+  setup: () => {
+    const { currentFileName, currentFile } = useData();
+
+    return {
+      currentFileName,
+    };
+  },
 });
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.title {
+  margin-bottom: 1.5em;
+}
+
+.meta-info {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 </style>
