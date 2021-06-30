3d2b9d9da94e9f3e0fed2c2f701cd38914ad3cb8
Marc <marcbaque1311@gmail.com>
2021-06-30 21:28:28 +0200

    [ REFACTOR ] move route handler fns to seperate file

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import { createFile, deleteFile, getFile, getFiles } from './handlers.ts';
 import { Application } from 'https://deno.land/x/abc@v1.3.3/mod.ts';
 import {
   HttpMethod,
@@ -7,7 +8,6 @@ import {
   CORSConfig,
   cors,
 } from 'https://deno.land/x/abc@v1.3.3/middleware/cors.ts';
-import { ensureDir } from 'https://deno.land/std@0.99.0/fs/mod.ts';
 import { green, bold, blue } from 'https://deno.land/std@0.99.0/fmt/colors.ts';
 
 console.info = (txt: string) =>
@@ -15,61 +15,22 @@ console.info = (txt: string) =>
 
 const app = new Application();
 
-const config: CORSConfig = {
+const corsConfig: CORSConfig = {
   allowOrigins: ['http://127.0.0.1:3000', 'http://localhost:3000'],
   allowMethods: [HttpMethod.Get, HttpMethod.Post, HttpMethod.Delete],
   allowHeaders: [Header.ContentType],
 };
-app.use(cors(config));
-
-console.info('Server listening on http://127.0.0.1:1234');
 
 app
+  .use(cors(corsConfig))
   .static('/', 'frontend')
   .static('/storage', 'storage')
   .file('/', 'frontend/index.html')
+  // routes
+  .post('/json', createFile)
+  .get('/files', getFiles)
+  .get('/file/:fileName', getFile)
+  .delete('/file/:fileName', deleteFile)
   .start({ port: 1234 });
 
-app.post('/json', async (c) => {
-  await ensureDir('./storage');
-
-  const body = (await c.body) as { fileName: string; file: string };
-  const fileName = `${body.fileName}.json`;
-  try {
-    await Deno.writeTextFile(`storage/${fileName}`, JSON.stringify(body.file));
-
-    console.info('File written to ' + `storage/${fileName}`);
-  } catch (e) {
-    return e.message;
-  }
-});
-
-app.get('/files', async (c) => {
-  await ensureDir('./storage');
-
-  const files: string[] = [];
-  for await (const dirEntry of Deno.readDir('./storage')) {
-    files.push(dirEntry.name.replace(/.json/, ''));
-  }
-  c.response.status = 200;
-  c.response.body = JSON.stringify(files);
-});
-
-app.get('/file/:fileName', async (c) => {
-  const fileName = c.params.fileName.replaceAll('%20', ' ');
-  const text = await Deno.readTextFile(`storage/${fileName}.json`);
-  const payload = { fileName, contents: text };
-
-  c.response.status = 200;
-  c.response.body = JSON.stringify(payload);
-});
-
-app.delete('/file/:fileName', async (c) => {
-  const fileName = c.params.fileName.replaceAll('%20', ' ');
-  try {
-    await Deno.remove(`storage/${fileName}.json`);
-    c.response.status = 200;
-  } catch (error) {
-    c.response.status = 500;
-  }
-});
+console.info('Server listening on http://127.0.0.1:1234');
---
+++ handlers.ts
@@ -0,0 +1,45 @@
+import { ensureDir } from 'https://deno.land/std@0.99.0/fs/mod.ts';
+import { HandlerFunc, Context } from 'https://deno.land/x/abc@v1.3.3/mod.ts';
+
+export const getFiles: HandlerFunc = async (c: Context) => {
+  await ensureDir('./storage');
+
+  const files: string[] = [];
+  for await (const dirEntry of Deno.readDir('./storage')) {
+    files.push(dirEntry.name.replace(/.json/, ''));
+  }
+
+  c.json(files, 200);
+};
+
+export const getFile: HandlerFunc = async (c: Context) => {
+  const fileName = c.params.fileName.replaceAll('%20', ' ');
+  const text = await Deno.readTextFile(`storage/${fileName}.json`);
+  const payload = { fileName, contents: text };
+
+  c.json(payload, 200);
+};
+
+export const createFile: HandlerFunc = async (c: Context) => {
+  await ensureDir('./storage');
+
+  const body = (await c.body) as { fileName: string; file: string };
+  const fileName = `${body.fileName}.json`;
+  try {
+    await Deno.writeTextFile(`storage/${fileName}`, JSON.stringify(body.file));
+
+    console.info('File written to ' + `storage/${fileName}`);
+  } catch (e) {
+    c.json({ error: e.message }, 500);
+  }
+};
+
+export const deleteFile: HandlerFunc = async (c: Context) => {
+  const fileName = c.params.fileName.replaceAll('%20', ' ');
+  try {
+    await Deno.remove(`storage/${fileName}.json`);
+    c.response.status = 200;
+  } catch (_error) {
+    c.response.status = 500;
+  }
+};
