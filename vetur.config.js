f45029300dcbbb1821e8ed48ee0e35042a36d7a8
Marc Baque <m.baque@inreal-tech.com>
2021-07-02 17:20:11 +0200

    [ UPDATE ] vetur config

--- vetur.config.js
+++ vetur.config.js
@@ -1,9 +1,4 @@
+/** @type {import('vls').VeturConfig} */
 module.exports = {
-  projects: [
-    './resources/src', // Shorthand for specifying only the project root location
-    {
-      package: './package.json',
-      tsconfig: './tsconfig.json',
-    },
-  ],
+  projects: ['./resources'],
 };
